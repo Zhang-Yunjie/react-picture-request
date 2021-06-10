6ba06e709b0b5a3d14c42b559186b28bb6d69449
Zhang-Yunjie <e0421414@u.nus.edu>
2021-06-10 18:06:16 +0800

    Create custom client(axios) for unsplash

---
+++ src/api/unsplash.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default xios.create({
+    baseURL : 'https://api.unsplash.com',
+    headers : {
+        Authorization: 'Client-ID 5hTLtlPDFAfdPC5sQCkC1OOEZaW9d6qfBRA2FMQs7XI'
+    }
+});
--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,27 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
 
 
 class App extends React.Component {
 
-    state = {};
+    state = { images: [] };
 
-    async onSearchSubmit(term) {
-        const response = await axios
-        .get('https://api.unsplash.com/search/photos',{
+    async onSearchSubmit(term){
+        const response = await unsplash
+        .get('/search/photos',{
             params: {query: term },
-            headers: {
-                Authorization: 'Client-ID 5hTLtlPDFAfdPC5sQCkC1OOEZaW9d6qfBRA2FMQs7XI'
-            }
         });
-        //.then(response => console.log(response.data.results));
 
-        console.log(response.data.results);
+        this.setState({ images: response.data.results});
     }
 
     render() {
         return (
             <div className="ui container" style={{marginTop:'30px'}}> 
-                <SearchBar onSubmit={this.onSearchSubmit}/>
+                <SearchBar onSubmit={term => this.onSearchSubmit(term)}/>
+                Found: {this.state.images.length} images
                 <ImageList />
             </div>
         );
