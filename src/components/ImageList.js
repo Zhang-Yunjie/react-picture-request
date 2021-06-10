e93323154be75918a5b9585722d34a2ff10f3847
Zhang-Yunjie <e0421414@u.nus.edu>
2021-06-10 19:37:53 +0800

    Add formatting for display of image list

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './ImageList.css';
 
 class ImageList extends React.Component {
     render() {
         const images = this.props.images.map(image => 
         <img alt={image.description} key={image.id} src={image.urls.regular} />);
 
-        return <div> {images} </div>;
+        return <div className="image-list"> {images} </div>;
     }
 }
 
