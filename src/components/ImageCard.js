3c243c627c3a971d26d76f9aea53909c058668e1
Zhang Yunjie <e0421414@u.nus.edu>
2021-06-14 00:50:10 +0800

    Dynamically assign span size for ImageCard

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,16 +4,23 @@ class ImageClass extends React.Component {
     constructor(props) {
         super(props);
         this.imageRef = React.createRef();
+        this.state = { spans : 0};
     }
 
-    componentDidMount(){
-        console.log(this.imageRef.current.clientHeight);
+    componentDidMount(){ // watch the loading of each imagecard object, call setspans upon loaded
+        this.imageRef.current.addEventListener('load',this.setSpans);
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 10);
+        this.setState({spans : spans })
     }
 
     render() {
         const {description, urls} = this.props.image;
         return (
-            <div>
+            <div style={{gridRowEnd: `span ${this.state.spans}`}}>
                 <img
                     ref={this.imageRef}
                     alt={description}
